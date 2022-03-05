dae4b26d9a762be8fb8eac5190ea68a878c80893
Brouney <piotreg98@gmail.com>
2022-03-05 19:41:59 +0100

    working slide 3

--- front/src/components/module0/Module_0_Slide_3_A.js
+++ front/src/components/module0/Module_0_Slide_3_A.js
@@ -1,18 +1,21 @@
 import React, {Component} from "react";
-
+import Module_0_Slide_2_Q from "./Module_0_Slide_2_Q";
+import NavigationButtons from "../templates/NavigationButtons";
 
 class Module_0_Slide_3_A extends Component {
 
     constructor(props){
         super(props)
         this.mainArea = props.mainArea
-        this.prev = props.prev;
-        this.next = null;
+        this.prev = <Module_0_Slide_2_Q mainArea={this.mainArea}></Module_0_Slide_2_Q>;
+        this.next = null   
         
     }
     render(){
         return(
-        <div>M03A</div>
+        <div>M03A
+            <NavigationButtons mainArea={this.mainArea} prev={this.prev} next={this.next}></NavigationButtons>
+        </div>
         )
     }
 
