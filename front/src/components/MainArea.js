382f39db6b0c1e3cb788b087d655fdc108af72c6
Brouney <piotreg98@gmail.com>
2022-03-05 19:34:16 +0100

    working beta !!!

--- front/src/components/MainArea.js
+++ front/src/components/MainArea.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react";
 import ListExercisePanel from './templates/ListExercisePanel'
-import Module_0_slides from "./module0/Module_0_slides";
 class MainArea extends Component {
 
     constructor(props){
@@ -13,7 +12,6 @@ class MainArea extends Component {
     render(){
         return(
         <div className='mainArea'>
-            <Module_0_slides mainArea={this}></Module_0_slides>
             <div className="row">
                     <ListExercisePanel mainArea={this}> </ListExercisePanel>
 
--- front/src/components/module0/Module_0_Slide_2_Q.js
+++ front/src/components/module0/Module_0_Slide_2_Q.js
@@ -1,13 +1,13 @@
 import React, {Component} from "react";
 import Module_0_Slide_3_A from "./Module_0_Slide_3_A";
 import NavigationButtons from "../templates/NavigationButtons";
-
+import Module_0_Slide_1_T from "./Module_0_Slide_1_T";
 class Module_0_Slide_2_Q extends Component {
 
     constructor(props){
         super(props)
         this.mainArea = props.mainArea
-        this.prev = props.prev;
+        this.prev = <Module_0_Slide_1_T mainArea={this.mainArea}></Module_0_Slide_1_T>;
         this.next = <Module_0_Slide_3_A prev={this} mainArea={this.mainArea}></Module_0_Slide_3_A>        
         
     }
