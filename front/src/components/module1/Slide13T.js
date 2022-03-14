bdb3dc70c56c1725d134c03cbf80c6576d03dd61
Brouney <piotreg98@gmail.com>
2022-03-14 18:56:02 +0100

    working quiz

--- front/src/components/module1/Slide13T.js
+++ front/src/components/module1/Slide13T.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import Slide12A from "./Slide12A";
 import NavigationButtons from "../templates/NavigationButtons";
 import { MODULE_1_SLIDES_COUNT } from '../templates/ListExercisePanel'
-
+import QuizTemplate from "../templates/QuizTemplate";
 
 class Slide13T extends Component {
 
@@ -11,12 +11,13 @@ class Slide13T extends Component {
         this.mainArea = props.mainArea
         this.prev = <Slide12A mainArea={this.mainArea}></Slide12A>;
         this.next = null   
-        
+        this.quizTemplate = React.createRef()
     }
     render(){
         return(
         <div>
             <h1>Moduł 1 Slajd 3</h1>
+            <QuizTemplate ref={this.quizTemplate} slide={this}></QuizTemplate>
             <NavigationButtons mainArea={this.mainArea} prev={this.prev} next={this.next} currentSlideCounter={3} slidesInModuleCounter={MODULE_1_SLIDES_COUNT}
                                current={this}
             ></NavigationButtons>
--- front/src/components/templates/quizTemplate.js
+++ front/src/components/templates/quizTemplate.js
@@ -1,18 +1,51 @@
 import React, {Component} from "react";
-
-
-class quizTemplate extends Component {
+var RED_BOOTSTRAP = "#d9534f"
+var GREEN_BOOTSTRAP = "#5cb85c"
+class QuizTemplate extends Component {
 
     constructor(props){
         super(props)
         this.mainArea = props.mainArea
+        this.slide = props.slide
+        console.log(this)
+        this.inproper1Answer = React.createRef();
+        this.inproper2Answer = React.createRef();
+        this.inproper3Answer = React.createRef();
+        this.properAnswer = React.createRef();
+        this.showAnswer = this.showAnswer.bind(this)
     }
+
+    showAnswer(){
+        console.log(this.inproper1Answer)
+        this.inproper1Answer.current.style.backgroundColor = RED_BOOTSTRAP
+        this.inproper2Answer.current.style.backgroundColor = RED_BOOTSTRAP
+        this.inproper3Answer.current.style.backgroundColor = RED_BOOTSTRAP
+        this.properAnswer.current.style.backgroundColor = GREEN_BOOTSTRAP
+    }
+
     render(){
         return(
-            <div></div>
+            <div className="quizTemplate items-center justify-center flex h-screen">
+                <div className="bg-black text-purple-800 p-10 ">
+                    <h2>
+                        Pytanie
+                    </h2>
+                </div>
+                <div className="quizbuttons">
+                    <ul>
+                    <button ref={this.inproper1Answer} onClick={this.showAnswer} className=" btn btn-primary p-4 " style={{margin: 20}}> odp1</button></ul>
+                    <ul>
+                    <button ref={this.inproper2Answer} onClick={this.showAnswer} className=" btn btn-primary p-4" style={{margin: 20}}> odp2 </button></ul>
+                    <ul>
+                    <button ref={this.inproper3Answer} onClick={this.showAnswer} className=" btn btn-primary p-4" style={{margin: 20}}>odp3</button></ul>
+                    <ul>
+                    <button ref={this.properAnswer } onClick={this.showAnswer} className=" btn btn-primary p-4" style={{margin: 20}}>odp4</button></ul>
+                </div>
+            </div>
+            
         )
     }
 
 }
 
-export default quizTemplate;
+export default QuizTemplate;
