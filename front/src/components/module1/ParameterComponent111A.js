ce0bf04227f8b86e6a39a7cad429cda1fd4a13a2
Marek <marcus1.2.3@wp.pl>
2022-04-25 10:23:09 +0200

    Slide1.11 done - standardowe kodowanie na N bitach

---
+++ front/src/components/module1/ParameterComponent111A.js
@@ -0,0 +1,170 @@
+import React, {Component} from "react";
+import { InputNumber } from 'antd';
+var Latex = require('react-latex');
+
+function howManyBitsNeededToRepresentInteger(value) {
+    if (value == 0) {
+        return 1
+    }
+    else {
+        return 1 + Math.floor(Math.log(value)/Math.log(2))
+    }
+}
+
+const zeroPad = (num, places) => String(num).padStart(places, '0')
+
+class ParameterComponent111A extends Component {
+
+    constructor(props){
+        super(props)
+
+        this.state = {
+            Umin: props.Umin ? props.Umin : 0,
+            Umax: props.Umax ? props.Umax : 7,
+            bity: props.bity ? props.bity : 3,
+            uKod: props.uKod ? props.uKod : 1,
+        }
+    }
+
+    handleChangeUmin = value => {
+        if (value == null) {
+            value = 0
+        }
+        value = Number.isInteger(value) ? value : Math.floor(value)
+
+        this.setState({
+            Umin: value,
+        });
+
+        if (value > this.state.uKod) {
+            this.setState({
+                uKod: value,
+            });
+        }
+    };
+
+    handleChangeUmax = value => {
+        if (value == null) {
+            value = 1
+        }
+        value = Number.isInteger(value) ? value : Math.floor(value)
+
+        let newBits = howManyBitsNeededToRepresentInteger(value)
+        this.setState({
+            Umax: value,
+            bity: newBits
+        });
+
+        if (value < this.state.uKod) {
+            this.setState({
+                uKod: value,
+            });
+        }
+    };
+
+    handleChangeBits = value => {
+        if (value == null) {
+            value = 1
+        }
+        value = Number.isInteger(value) ? value : Math.floor(value)
+        this.setState({
+            bity: value,
+        });
+
+        let maxPossibleEncodedValue = 2**value - 1
+        if (maxPossibleEncodedValue < this.state.Umax) {
+            this.setState({
+                Umin: 0,
+                Umax: maxPossibleEncodedValue,
+                uKod: 0
+            });
+        }
+    };
+
+    handleChangeUkod = value => {
+        if (value == null) {
+            value = this.state.Umin
+        }
+
+        // TODO kodujemy tylko liczby calkowite?
+        // TODO umin=12 umax=31 ukod=21 = liczba binarna z ulamkiem, czy mozemy tak kodowac? dopytac promot
+        // value = Number.isInteger(value) ? value : Math.floor(value)
+
+        this.setState({
+            uKod: value,
+        });
+    };
+
+    render(){
+        const { Umin, Umax, bity, uKod } = this.state;
+        let xKod = (uKod - Umin) * (2**bity - 1) / (Umax - Umin)
+
+        return(
+            <div style={{margin: "10px"}}>
+                <h5><li>
+                <label>
+                    U_min
+                    <InputNumber
+                        min={0}
+                        max={this.state.Umax-1}
+                        defaultValue={this.state.Umin}
+                        style={{ margin: '0 16px' }}
+                        value={typeof Umin === 'number' ? Umin : this.state.Umin}
+                        onChange={this.handleChangeUmin}
+                        step={this.step ? this.step : 1}
+                    />
+                </label>
+
+                <label>
+                    U_max
+                    <InputNumber
+                        min={this.state.Umin+1}
+                        max={1023}
+                        defaultValue={15}
+                        style={{ margin: '0 16px' }}
+                        value={typeof Umax === 'number' ? Umax : this.state.Umax}
+                        onChange={this.handleChangeUmax}
+                        step={this.step ? this.step : 1}
+                    />
+                </label>
+
+                <label>
+                    Liczba bitów
+                    <InputNumber
+                        min={1}
+                        max={10}
+                        defaultValue={1}
+                        style={{ margin: '0 16px' }}
+                        value={typeof bity === 'number' ? Math.floor(bity) : this.state.bity}
+                        onChange={this.handleChangeBits}
+                        step={this.step ? this.step : 1}
+                    />
+                </label>
+
+                <label>
+                    u kodowane
+                    <InputNumber
+                        min={this.state.Umin}
+                        max={this.state.Umax}
+                        defaultValue={this.state.Umin}
+                        style={{ margin: '0 16px' }}
+                        value={typeof uKod === 'number' ? uKod : this.state.uKod}
+                        onChange={this.handleChangeUkod}
+                        step={this.step ? this.step : 1}
+                    />
+                </label>
+
+                x = {Number.isInteger(xKod)
+                ? xKod
+                : xKod.toFixed(3)}
+                <Latex>{"$${_{dec}}$$"}</Latex>
+
+                 ({zeroPad(xKod.toString(2), bity)}<Latex>{"$${_{bin}}$$"}</Latex>)
+                </li></h5>
+            </div>
+        )
+    }
+
+}
+
+export default ParameterComponent111A;
--- front/src/components/module1/Slide111A.js
+++ front/src/components/module1/Slide111A.js
@@ -3,6 +3,8 @@ import Slide110A from "./Slide110A";
 import Slide112A from "./Slide112A";
 import NavigationButtons from "../templates/NavigationButtons";
 import { MODULE_1_SLIDES_COUNT } from '../templates/ListExercisePanel'
+import ParameterComponent111A from "./ParameterComponent111A";
+var Latex = require('react-latex');
 
 class Slide111A extends Component {
 
@@ -11,16 +13,56 @@ class Slide111A extends Component {
         this.mainArea = props.mainArea
         this.prev = <Slide110A mainArea={this.mainArea}></Slide110A>
         this.next = <Slide112A prev={<Slide111A></Slide111A>} mainArea={this.mainArea}></Slide112A>
-        this.title = 'tytul'
+        this.title = 'Standardowe kodowanie dowolnej liczby na N bitach'
+
+        this.addParameter = React.createRef();
+        this.deleteParameter = React.createRef();
+
         this.state = {
-            backpackCurrentWeight: 0
+            parameters: [
+                <ParameterComponent111A Umax={7} bity={3}/>,
+                <ParameterComponent111A Umax={15} bity={4}/>,
+                <ParameterComponent111A Umax={31} bity={5}/>,
+            ]
         }
+    }
+
+    onClickAddParameter = () => {
+        this.setState(prevState => ({
+            parameters: [...prevState.parameters, <ParameterComponent111A Umax={15} bity={4}/>]
+        }))
+    }
+
+    onClickDeleteParameter = () => {
+        var parametersAfterDeletion = [...this.state.parameters];
+        var lastElement = parametersAfterDeletion.length-1
+
+        parametersAfterDeletion.splice(parametersAfterDeletion[lastElement], 1);
+        this.setState({parameters: parametersAfterDeletion});
     }
 
     render(){
         return(
             <div>
                 <h1>{this.title}</h1>
+                <div className="row">
+                    <div className="col-3">
+                    <h4>
+                        {/* <Latex>{"${\\left\\{\\begin{matrix}  \\end{matrix}\\right.}$"}</Latex> */}
+                        <div style={{margin: "20px"}}><Latex>{"$${x = \\frac{u - U_{min}}{U_{max} - U_{min}}(2^l - 1)}$$"}</Latex><br></br></div>
+                        <div style={{margin: "20px"}}><Latex>{"${u = U_{min} + \\frac{U_{max} - U_{min}}{2^l - 1}x}$"}</Latex></div>
+                    </h4>
+                    </div>
+                    <div className="col">
+                    <button ref={ref => this.addParameter = ref} type="submit" className="btn btn-success col-2 m-1" onClick={this.onClickAddParameter}>Dodaj parametr</button>
+                    <button ref={ref => this.deleteParameter = ref} type="submit" className="btn btn-warning col-2 m-1" onClick={this.onClickDeleteParameter}>Usuń parametr</button>
+                    </div>
+                </div>
+
+                <div className="col-8" style={{textAlign:"center"}}><h3>PARAMETRY</h3></div>
+                <ol>
+                    {this.state.parameters.map(component => component)}
+                </ol>
 
                 <NavigationButtons mainArea={this.mainArea} prev={this.prev} next={this.next} currentSlideCounter={11} slidesInModuleCounter={MODULE_1_SLIDES_COUNT}
                                 current={this}
